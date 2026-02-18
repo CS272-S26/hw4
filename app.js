d685bf9ab6b1aeacdf3edf1909c5b7237bc58668
Cole Nelson <ctnelson2@wisc.edu>
2026-02-18 09:41:54 -0600

    hw4

---
+++ app.js
@@ -0,0 +1,10 @@
+
+function checkout() {
+    const numDrinks = getNumDrinks();
+    const pricePerDrink = getPricePerDrink();
+    const tipPercent = getTip();
+    const hasLoyalty = getLoyaltyStatus();
+
+    // TODO Calculate the total and alert the user of the results!
+    alert("I should check that all values have been entered do the calculation!")
+}
